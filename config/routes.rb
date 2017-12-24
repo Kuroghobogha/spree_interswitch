d8c2e1d0c474a0d42dad459b629fd7accf4cf3af
Douglas Kendyson <kendyson@kendyson.com>
2017-12-24 06:57:59 +0100

    Switched to get routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.add_routes do
-  post '/interswitch', :to => "interswitch#index", :as => :interswitch_proceed
-  post '/interswitch/confirm', :to => "interswitch#confirm", :as => :interswitch_confirm
-  post '/interswitch/cancel', :to => "interswitch#cancel", :as => :interswitch_cancel
+  get '/interswitch', :to => "interswitch#index", :as => :interswitch_proceed
+  get '/interswitch/confirm', :to => "interswitch#confirm", :as => :interswitch_confirm
+  get '/interswitch/cancel', :to => "interswitch#cancel", :as => :interswitch_cancel
 end
